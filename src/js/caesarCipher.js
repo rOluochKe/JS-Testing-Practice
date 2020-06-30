5a62a1aeba54af2e5fe45400e2cb6dd6dcd6c9be
leonmezu1 <leo.mez@outlook.com>
2020-06-30 17:32:31 -0500

    Caesar Cipher PASSED

---
+++ src/js/caesarCipher.js
@@ -0,0 +1,22 @@
+const caesarCipher = (string, shift) => {
+  const wordsArray = string.split(' ');
+  let cipher = '';
+  wordsArray.forEach(word => {
+    [...word].forEach((char, i) => {
+      const code = word.charCodeAt(i);
+      if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        if ((code + shift > 122) || (code + shift > 90 && code < 97)) {
+          cipher += String.fromCharCode(code + shift - 26);
+        } else {
+          cipher += String.fromCharCode(code + shift);
+        }
+      } else {
+        cipher += char[i];
+      }
+    });
+    cipher += ' ';
+  });
+  return cipher.trim();
+};
+
+module.exports = caesarCipher;
