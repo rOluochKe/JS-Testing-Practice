389caff21b5641f96389949f6d7cd3bf9151252f
leonmezu1 <leo.mez@outlook.com>
2020-06-30 18:58:23 -0500

    added parenthesis in required line

--- src/js/caesarCipher.js
+++ src/js/caesarCipher.js
@@ -6,7 +6,7 @@ const caesarCipher = (string, shift) => {
       const code = word.charCodeAt(i);
       if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
         if ((code + shift > 122) || (code + shift > 90 && code < 97)) {
-          cipher += String.fromCharCode(code + shift - 26);
+          cipher += String.fromCharCode(code + (shift - 26));
         } else {
           cipher += String.fromCharCode(code + shift);
         }
